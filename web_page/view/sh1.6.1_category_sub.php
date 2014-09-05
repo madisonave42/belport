bf16da067f8beb7b7087856b0d9fa5abf99cc52b
donghyuncho <sectionp@gmail.com>
2014-09-05 12:13:55 +0900

    샵 카테고리 대,중 페이지 추가

---
+++ web_page/view/sh1.6.1_category_sub.php
@@ -0,0 +1,9 @@
+<?php
+	require('include/head.php');
+	
+	require('../html/include/gnb.html');
+	
+	require('../html/content/sh1.6.1_category_sub.html');	
+
+	require('../html/include/footer.html');
+?>
