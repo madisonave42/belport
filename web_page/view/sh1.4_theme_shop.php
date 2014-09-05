960b58f7f90ba8a739494e153b6cdedbe8913e53
moon <ministori@gmail.com>
2014-09-05 12:20:36 +0900

    theme shop

---
+++ web_page/view/sh1.4_theme_shop.php
@@ -0,0 +1,9 @@
+<?php
+	require('include/head.php');
+	
+	require('../html/include/gnb.html');
+	
+	require('../html/content/sh1.4_theme_shop.html');	
+
+	require('../html/include/footer.html');
+?>
