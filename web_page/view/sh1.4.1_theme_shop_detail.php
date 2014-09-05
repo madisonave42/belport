a68bd5170dc95753653d6528c397811d07fb8de4
burning5 <burning05@gmail.com>
2014-09-05 16:25:33 +0900

    개별 theme shop 추가

---
+++ web_page/view/sh1.4.1_theme_shop_detail.php
@@ -0,0 +1,9 @@
+<?php
+	require('include/head.php');
+	
+	require('../html/include/gnb.html');
+	
+	require('../html/content/sh1.4.1_theme_shop_detail.html');	
+
+	require('../html/include/footer.html');
+?>
