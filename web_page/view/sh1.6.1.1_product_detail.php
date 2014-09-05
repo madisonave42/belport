25ee496a30d19fc316cbfc5af905c354108e5000
donghyuncho <sectionp@gmail.com>
2014-09-05 17:36:20 +0900

    샵 - 제품 상세 추가

---
+++ web_page/view/sh1.6.1.1_product_detail.php
@@ -0,0 +1,9 @@
+<?php
+	require('include/head.php');
+	
+	require('../html/include/gnb.html');
+	
+	require('../html/content/sh1.6.1.1_product_detail.html');	
+
+	require('../html/include/footer.html');
+?>
