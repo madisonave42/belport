b4a0368f50061f0f61829d3e65c5ea7539742457
donghyuncho <sectionp@gmail.com>
2014-09-16 14:03:38 +0900

    개인정보보호방침 & 이용약관 추가

---
+++ web_page/view/gr0.1_agreement.php
@@ -0,0 +1,9 @@
+<?php
+	require('include/head.php');
+	
+	require('../html/include/gnb.html');
+	
+	require('../html/content/gr0.1_agreement.html');
+
+	require('../html/include/footer.html');
+?>
---
+++ web_page/view/gr0.1_privacy_policy.php
@@ -0,0 +1,9 @@
+<?php
+	require('include/head.php');
+	
+	require('../html/include/gnb.html');
+	
+	require('../html/content/gr0.1_privacy_policy.html');
+
+	require('../html/include/footer.html');
+?>
