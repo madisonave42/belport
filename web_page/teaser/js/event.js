8623ae07bd4c779eeb045cef667d203812872333
moon <ministori@gmail.com>
2014-09-18 17:19:04 +0900

    티저 수정

--- web_page/teaser/js/event.js
+++ web_page/teaser/js/event.js
@@ -30,14 +30,12 @@ $(function(){
 		
 		
 		
-		$currentBanner.stop().animate({top:-145, opacity:0}, 500);
+		$currentBanner.stop().animate({top:-145, opacity:0}, 1000);
 		$nextBanner.css({top:315});
-		$nextBanner.stop().animate({top:85, opacity:1}, 500);
+		$nextBanner.stop().animate({top:85, opacity:1}, 1000);
 		
 		currentIndex = nextIndex;
 	}
-
-
 
 	// mobile
 	if( ua.toLowerCase().indexOf( 'mobile' ) > 0 ){
@@ -48,9 +46,10 @@ $(function(){
 
 	//rolling
 	init();
-	tID = setInterval(setIndex, 5000);
+	tID = setInterval(setIndex, 10000);
 	
 	$('.arrow').on('click', function(){
+		if($('.text_area').is(':animated')){return false;}
 		clearInterval(tID);
 		setIndex();
 	});
