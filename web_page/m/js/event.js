86132a153a04098eb0afe5ff27aa468104d030d8
moon <ministori@gmail.com>
2014-10-24 16:25:38 +0900

    이벤트 - 제품 선택 페이지 제작

--- web_page/m/js/event.js
+++ web_page/m/js/event.js
@@ -1,3 +1,17 @@
-/**
- * @author Administrator
- */
+$(function(){
+	
+	$(window).on('load', function(){
+		$('.ui-loader').remove();
+	});
+	
+	$('.prd').on('swipeleft', function(e){
+		e.preventDefault();
+		$(this).stop().animate({left:-$(this).width()/4});
+	});
+
+	$('.prd').on('swiperight', function(e){
+		e.preventDefault();
+		$(this).stop().animate({left:0});
+	});
+	
+});
