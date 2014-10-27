49d792f6bb28546f1ba06c608301a2facee029a2
junsik <burning05@gmail.com>
2014-10-27 14:58:02 +0900

    폰트(RixSGo M, font-weight) 수정 브랜드위크 완료팝업 추가

--- web_page/js/event.js
+++ web_page/js/event.js
@@ -470,6 +470,16 @@ $(function(){
  		$('.layer-pop').hide();
  		$('.layer-pop .pop').hide();
  	});
+	
+	// 임시 완료팝업보이기
+	$('.brandweek-complete').height($(document).height()).show();
+	
+	// 참여완료 팝업
+	$('.brandweek-complete .close').on('click', function(e){
+		e.preventDefault();
+		
+		$('.brandweek-complete').hide();
+	});
  	
  })();
  
